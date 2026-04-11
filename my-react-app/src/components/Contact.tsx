a5cd07b008d8addb90d4110e3adb00892e918bb2
jerome sauve <jerome.sauve.etu@univ-lille.fr>
2026-04-11 21:45:31 +0200

    structure contact

--- my-react-app/src/components/Contact.tsx
+++ my-react-app/src/components/Contact.tsx
@@ -1,7 +1,12 @@
 const Contact = () => {
 
     return (
-        <h2> Contact </h2>
+        <div className="contactList">
+            <a href="mailto:jerome.sauve.etu@univ-lille.fr" target="_blank" className="contactLink"><p>M'envoyer un mail.</p></a>
+            <a href = "https://www.linkedin.com/in/jérôme-sauvé-19056119a/" target="_blank" className="contactLink"><p>Ma page LinkedIn</p></a>
+            <a href="https://gitlab.univ-lille.fr/jerome.sauve.etu" target="_blank" className="contactLink"><p>Ma page Gitlab</p></a>
+            <a href="https://github.com/JeromeSoV" target="_blank" className="contactLink"><p>Ma page Github</p></a>
+        </div>
     );
 }
 
