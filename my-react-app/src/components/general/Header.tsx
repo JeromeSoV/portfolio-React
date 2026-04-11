589595954efd10ee1f6563e7a9b5f5155c8359f8
jerome sauve <jerome.sauve.etu@univ-lille.fr>
2026-04-11 21:23:33 +0200

    structure header

--- my-react-app/src/components/general/Header.tsx
+++ my-react-app/src/components/general/Header.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
 
     return (
-        <h1>Header</h1>
+        <header className="appHeader">
+            <div className="headerTop">
+                <h1>Jérôme Sauvé</h1>
+                <Link to="/">
+                    <img src="../../assets/react.svg" alt="Logo personnel"></img>
+                </Link>
+            </div>
+            <nav className="headerNavigation">
+                <Link to="/">Accueil</Link>
+                <Link to="/about">Informations</Link>
+                <Link to="/projets">Projets</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/docs/Jerome_Sauve.pdf" target="_blank">Télecharger CV</Link>
+            </nav>
+        </header>
     );
 }
 
