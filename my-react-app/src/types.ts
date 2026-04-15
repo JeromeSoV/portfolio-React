bee64886ecd05f503dad8222ff75e059dd6f3303
jerome sauve <jerome.sauve.etu@univ-lille.fr>
2026-04-15 15:46:31 +0200

    maj package, qjout type et values

--- my-react-app/src/types.ts
+++ my-react-app/src/types.ts
@@ -1,3 +1,27 @@
 export type ProjetDetailProp = {
     slug : string;
 }
+
+export type ProjectDetail = {
+    title : string;
+    content : string;
+}
+
+export type ProjectThumbnail = {
+    slug : string;
+    title : string;
+    summary : string;
+    image : string;
+    tags : string[];
+}
+
+export type ProjectContent = ProjectData & {
+    content : string;
+}
+export type ProjectData = {
+    [key: string]: any;
+}
+
+export type ProjetTagProps = {
+    type : string;
+}
