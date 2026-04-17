cb570983083a7df995d66334fb2a10f4514dde66
jerome.sauve.etu <jerome.sauve.etu@univ-lille.fr>
2026-04-17 12:06:18 +0200

    Structure ProjetDetail

--- my-react-app/src/components/projets/ProjectTag.tsx
+++ my-react-app/src/components/projets/ProjectTag.tsx
@@ -2,11 +2,10 @@
 import projets from "../../styles/projets.module.css";
 import type {ProjetTagProps} from "../../types.ts";
 import tagColors from "../../utils/values.ts";
-
+const tagStyle = (type : string) => {
+    return `${projets.tag} backgroundColor:`+`${(tagColors.has(type)) ? tagColors.get(type) : "white"}`
+}
 const ProjectTag = ({type} : ProjetTagProps)=>{
-    const tagStyle = (type : string) => {
-        return `${projets.tag} backgroundColor:`+`${(tagColors.has(type)) ? tagColors.get(type) : "white"}`
-    }
     return(
         <div className={tagStyle(type)}>
             {type}
--- my-react-app/src/components/projets/ProjetDetail.tsx
+++ my-react-app/src/components/projets/ProjetDetail.tsx
@@ -1,13 +1,35 @@
-import { type ProjetDetailProp } from "../../types.ts";
+import {type ProjectDetail, type ProjetDetailProp} from "../../types.ts";
 import {parseMdContent} from "../../utils/ArchiveCreator/mdParser.ts";
+import {useEffect, useState} from "react";
+import projet from "../../styles/projet.module.css";
+import {Link} from "react-router-dom";
 
-
+const initProjectDetail : ProjectDetail={
+    content: "",
+    title: ""
+}
 const ProjetDetail = ({ slug } : ProjetDetailProp) =>{
-    fetch("/archive/"+slug+"/index.json")
-        .then(res => res.json())
-        .then(data => console.log(data));
+    const [detail,setDetail]=useState(initProjectDetail);
+
+    useEffect(()=>{
+        fetch(`/public/archive/${slug}/index.json`)
+            .then(res => res.json())
+            .then(data => setDetail(data))
+    },[]);
+
     return(
-        <h2>Projet - {slug}</h2>
+        <main className={projet.detailMain}>
+            <div className={projet.parents}>
+                <Link to="/" className={projet.parentLink}>Home</Link>
+                <span> {">>"} </span>
+                <Link to="/projets" className={projet.parentLink}>Catalogue</Link>
+                <span> {">>"} </span>
+                <Link to={"/projets/"+slug} className={projet.parentLink}>{"projet "+slug}</Link>
+            </div>
+            <div className={projet.content} dangerouslySetInnerHTML={{__html :detail.content}}>
+            </div>
+
+        </main>
     )
 }
 
